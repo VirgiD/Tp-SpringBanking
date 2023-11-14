8624ba3c73dfa0fa5b4f765c0aebf1946f738005
VirgiD <virginiadanitz@gmail.com>
2023-11-14 18:04:43 -0300

    tp

--- src/main/java/com/cac/springbankinggrupo4/Repositories/UsuarioRepositorio.java
+++ src/main/java/com/cac/springbankinggrupo4/Repositories/UsuarioRepositorio.java
@@ -1,4 +1,9 @@
 package com.cac.springbankinggrupo4.Repositories;
 
-public interface UsuarioRepositorio {
+import com.cac.springbankinggrupo4.Models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario,Long> {
 }
+
