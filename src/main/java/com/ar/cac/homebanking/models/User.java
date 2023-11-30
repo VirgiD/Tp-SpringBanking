4892f5f1a4b6e5e51d5da14bfe0c92304e2eab1c
claudia <claudiachauque9@gmail.com>
2023-11-30 10:49:07 -0300

    cambios de codigo base por los del profe

---
+++ src/main/java/com/ar/cac/homebanking/models/User.java
@@ -0,0 +1,37 @@
+package com.ar.cac.homebanking.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mail")
+    private String email;
+
+    @Column(name = "contrasena")
+    private String password;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column(name = "apellido")
+    private String surname;
+
+    private String dni;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Account> accounts;
+}
