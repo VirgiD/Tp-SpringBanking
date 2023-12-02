261b73b37fba72005e52a0eb83ae73bb919e3ff7
FrancoDujanoff <fdujanoff@gmail.com>
2023-12-02 16:10:05 -0300

    Tarea comenzada

---
+++ src/main/java/com/ar/cac/homebanking/models/Atm.java
@@ -0,0 +1,29 @@
+package com.ar.cac.homebanking.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Atm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "atm_id")
+
+    private Long id;
+
+    private Long origin;
+
+    private Date date;
+
+    private BigDecimal amount;
+
+
+}
