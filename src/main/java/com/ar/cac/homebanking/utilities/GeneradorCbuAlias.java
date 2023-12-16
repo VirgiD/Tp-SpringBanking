bab6b89546916aed6c0007c29a3f63b5ca2d812d
VirgiD <virginiadanitz@gmail.com>
2023-12-16 11:56:52 -0300

    correccion logica para no repetir palabras en la creacion del alias

--- src/main/java/com/ar/cac/homebanking/utilities/GeneradorCbuAlias.java
+++ src/main/java/com/ar/cac/homebanking/utilities/GeneradorCbuAlias.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GeneradorCbuAlias {
     private static final List<String> PALABRAS = Arrays.asList("sol", "luna", "estrella", "rio", "montaña", "cabaña", "foco", "pais",
@@ -17,29 +18,41 @@ public class GeneradorCbuAlias {
         Random random = new Random();
         StringBuilder alias = new StringBuilder();
 
+        List<String> palabrasDisponibles = new ArrayList<>(PALABRAS);
+
         for (int i = 0; i < 3; i++) {
-            String palabraAleatoria = obtenerPalabraAleatoriaNoUtilizada(random);
+            if (palabrasDisponibles.isEmpty()) {
+                // Reinicia las palabras disponibles si se agotan
+                palabrasDisponibles.addAll(PALABRAS);
+                PALABRAS_UTILIZADAS.clear();
+            }
+
+            String palabraAleatoria = obtenerPalabraAleatoriaNoUtilizada(random, palabrasDisponibles);
 
             alias.append(palabraAleatoria);
 
             PALABRAS_UTILIZADAS.add(palabraAleatoria);
+
+            palabrasDisponibles.remove(palabraAleatoria);
 
             if (i < 2) {
                 alias.append(".");
             }
         }
 
-        // Reinicia las palabras utilizadas si se han agotado todas
-        if (PALABRAS_UTILIZADAS.size() == PALABRAS.size()) {
-            PALABRAS_UTILIZADAS.clear();
-        }
-
         return alias.toString();
     }
 
+    private static String obtenerPalabraAleatoriaNoUtilizada(Random random, List<String> palabrasDisponibles) {
+        String palabraAleatoria = palabrasDisponibles.get(random.nextInt(palabrasDisponibles.size()));
+        return palabraAleatoria;
+    }
+
     private static String obtenerPalabraAleatoriaNoUtilizada(Random random) {
-        List<String> palabrasNoUtilizadas = new ArrayList<>(PALABRAS);
-        palabrasNoUtilizadas.removeAll(PALABRAS_UTILIZADAS);
+        List<String> palabrasNoUtilizadas = PALABRAS.stream()
+                .filter(palabra -> !PALABRAS_UTILIZADAS.contains(palabra))
+                .collect(Collectors.toList());
+
         return palabrasNoUtilizadas.get(random.nextInt(palabrasNoUtilizadas.size()));
     }
 
